6c474e54f86c811275df7f48b89ec6b7e7d7cc39
Lucas Pioner Sartori <lucasartori2020@gmail.com>
2025-09-02 14:40:43 -0700

    make build

---
+++ index.4404bf81.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("contactForm").addEventListener("submit",function(t){t.preventDefault()})});
+//# sourceMappingURL=index.4404bf81.js.map
